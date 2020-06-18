462e01856dc9e4f78f86161e764662af36c0d8b2
JaessScript <jaess.script@gmail.com>
2020-06-18 12:04:58 +0200

    Improve Next link

--- sketch.js
+++ sketch.js
@@ -1,14 +1,27 @@
 let canvas;
+let next;
 
 // Resive the canvas when the size of the browser changes
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
+	setup();
 }
 
 function setup() {
 	canvas = createCanvas(windowWidth, windowHeight);
 	canvas.position(0, 0);
 	canvas.style('z-index', '-1');
+
+	next = select('#next');
+	next.position(windowWidth / 2, 5);
+	next.style('font-size', '1.5em');
+	setInterval(changeColor, 500);
+}
+
+function changeColor() {
+	let colors = ['Red', 'Orange', 'Yellow', 'MediumSpringGreen', 'RoyalBlue', 'Purple', 'Pink', 'LightCyan'];
+	let col = random(colors);
+	next.style('background-color', col);
 }
 
 function draw() {
