0a014427b1136e9d506268873e4a37c118f84b00
JaessScript <jaess.script@gmail.com>
2020-06-18 19:37:21 +0200

    Add image of German map looping down

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 let canvas;
 let next;
 
+let germany;
+let x_ge;
+let y_ge;
+
 // Resive the canvas when the size of the browser changes
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
@@ -16,12 +20,27 @@ function setup() {
 	next.position(windowWidth / 2, 5);
 	next.style('font-size', '1.5em');
 	setInterval(changeColor, 500);
+
+	germany = select('#germany');
+	x_ge = windowWidth / 3;
+	y_ge = 0;
+	setInterval(changePositionGermany, 300);
+
 }
 
 function changeColor() {
 	let colors = ['Red', 'Orange', 'Yellow', 'MediumSpringGreen', 'RoyalBlue', 'Purple', 'Pink', 'LightCyan'];
 	let col = random(colors);
 	next.style('background-color', col);
+}
+
+function changePositionGermany() {
+	germany.position(x_ge, y_ge);
+	if(y_ge<windowHeight){
+		y_ge += 10;
+	} else {
+		y_ge = 0;
+	}
 }
 
 function draw() {
